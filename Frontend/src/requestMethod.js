e624000294629f2d6cf4f413705dc08a71d9fb39
Chunnu0017 <chunnukumar0017@gmail.com>
2024-12-31 00:20:42 +0530

    kfkf

--- Frontend/src/requestMethod.js
+++ Frontend/src/requestMethod.js
@@ -3,5 +3,5 @@ import axios from "axios";
 const BASE_URL = "https://expense-tracker-olho.onrender.com"
 
 export const publicRequest = axios.create({
-    baseURL:BASE_URL,
+    baseURL:BASE_URL, 
 })
