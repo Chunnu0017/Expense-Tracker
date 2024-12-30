b0d41384dd7714cffc2944e846c1a959e97fb86d
Chunnu0017 <chunnukumar0017@gmail.com>
2024-12-31 00:13:55 +0530

    added backend url

--- Frontend/src/requestMethod.js
+++ Frontend/src/requestMethod.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000"
+const BASE_URL = "https://expense-tracker-olho.onrender.com"
 
 export const publicRequest = axios.create({
     baseURL:BASE_URL,
