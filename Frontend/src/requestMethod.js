a03146d94e2828fc3c8980e91a08794e84e19f59
Chunnu0017 <146914643+Chunnu0017@users.noreply.github.com>
2024-12-31 11:02:45 +0530

    Update requestMethod.js

--- Frontend/src/requestMethod.js
+++ Frontend/src/requestMethod.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://expense-tracker-olho.onrender.com"
+const BASE_URL = "https://expense-tracker-backend-bwh1.onrender.com"
 
 export const publicRequest = axios.create({
     baseURL:BASE_URL,
